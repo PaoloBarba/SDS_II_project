be3c95f5eb65c3dfd3eb3e7a0ec0b0879a720f57
PaoloBarba <114430394+PaoloBarba@users.noreply.github.com>
2023-06-24 15:00:51 +0200

    updating preferred foot

--- datates_engeneering.R
+++ datates_engeneering.R
@@ -1,4 +1,16 @@
 rm(list = ls())
 
-clean_data <- read.csv("csv_file/clean_data.csv")
-View(clean_data)
+shots_data <- read.csv("csv_file/clean_data.csv")
+
+shots_data
+
+library(dplyr)             # Load the package
+df_grouped <- group_by(shots_data, player)
+
+df_grouped$bodypart
+
+
+df_summary <- summarize(df_grouped, pref_foot = names(which.max(table(bodypart))))
+df <- mutate(shots_data, prefered_type = df_summary$pref_foot[match(shots_data$player, df_summary$player)])
+
+
