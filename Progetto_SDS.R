7403a9b8a495b42287620463db135947c0147e8a
PaoloBarba <114430394+PaoloBarba@users.noreply.github.com>
2023-06-24 08:33:41 +0200

    built dataset

--- Progetto_SDS.R
+++ Progetto_SDS.R
@@ -1,4 +1,25 @@
-install.packages("jsonlite")
+rm(list = ls())
+library(dplyr)
 library('jsonlite')
-json_data <- fromJSON('data/events/7471.json')
-json_data
+
+path <- "data/events"
+files <- dir(path, pattern = "*.json")
+path_file <- paste0(path,"/",files)
+
+data_list <- list()
+
+for (file_path in path_file) {
+  data <- fromJSON(file_path)
+  data_shot <-  data$shot[is.na(data$shot$outcome) == FALSE,]
+  if (dim(data_shot)[2] == 11){
+  data_list[[file_path]] <- data_shot
+  }
+    
+}
+
+
+combined_df <- do.call(bind_rows, data_list)
+shot_dataset <- combined_df
+
+load(file = "shotdataset.RData" )
+
---
+++ datates_engeneering.R
@@ -0,0 +1,3 @@
+library(readr)
+clean_data <- read_csv("clean_data.csv")
+View(clean_data)
