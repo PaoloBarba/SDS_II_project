9f898892241517b6317901594c476411a94c1213
PaoloBarba <114430394+PaoloBarba@users.noreply.github.com>
2023-06-27 12:45:01 +0200

    jags

--- Progetto_SDS.R
+++ Progetto_SDS.R
@@ -21,30 +21,31 @@ K <- ncol(barca_shot)
 colnames(barca_shot)
 
 
-library("lme4")
-model = glmer(as.factor(goal) ~ (1|player) + shot_distance + shot_angle + bodypart +
-        technique + first_touch + prefered_type + inside_18 + time , data = barca_shot, 
-        family = 'binomial')
 
 
 
+# Model -----------------------------------------------------------------------------------------------------------------------------------------
+library(lme4)
+library(R2jags)
 
-round(fixef(model), 4)
-ranef(model)
-data.frame(VarCorr(model))
+# Fit the generalized linear mixed effects model (GLMM)
+model <- glmer(as.factor(goal) ~ (1|player) + scale(shot_distance) + scale(shot_angle) + bodypart +
+                 technique + first_touch + prefered_type + inside_18 + scale(time), 
+               data = barca_shot, family = binomial)
 
 
 
-cat("model {
-for(k in 1:K){ # prior for beta
-	beta[k] ~ dnorm(0,0.001)
+# Obtain the number of observations, groups, and parameters
+n <- nrow(barca_shot)
+G <- length(unique(barca_shot$player))
+K <- 12  # Number of fixed effects parameters
+
+# Write JAGS model specification to a file
+model_code <- "
+model {
+  for(k in 1:K){ 
+	beta[k] ~ dnorm(0,0.001)   # prior for beta
 }	
-
-tau.e ~ dgamma(0.001,0.001) # prior for the within group variance
-sigma.e <- 1/sqrt(tau.e)
-
-tau.b0 ~ dgamma(0.001,0.001) # prior variance for the between group variance
-sigma.b0 <- 1/sqrt(tau.b0)
 
 # Statistical (conditional) model
 for (j in 1:G) {
@@ -52,29 +53,56 @@ for (j in 1:G) {
   b0[j] ~ dnorm(0, tau.b0) # random intercept for each ranked player
 }
 
-for(i in 1:n){ # likelihood function !!
-	mu[i] <- beta[1] + b0[barca_shot$player[i]] + beta[2] * distance[i] + beta[3] * angle[i] + beta[4] * body_part[i] + 
-	         beta[5] * tecnique[i] + beta[6] * first_touch[i] + beta[7] * preferred_type[i] + beta[8] * inside_18[i] +  beta[9] time[i]
-	y[i] ~ dnorm(mu[i], tau.e)
-}
 
-}", file = "lmm.model.txt", fill = TRUE)
+  for (i in 1:n) {
+    mu[i] <- beta[1] + b0[player[i]] + beta[2] * shot_distance[i] + beta[3] * shot_angle[i] + beta[4] * bodypart[i] + 
+              beta[5] * technique[i] + beta[6] * first_touch[i] + beta[7] * prefered_type[i] + 
+              beta[8] * inside_18[i] + beta[9] * time[i]
+    y[i] ~ dbin(p[i], 1)  # Likelihood function with binomial distribution
+    p[i] <- 1 / (1 + exp(-mu[i]))  # Link function (logit)
+  }
+}"
 
-inits = list( list(b0 = t(ranef(model)$rank)[1:3], 
-                   beta = as.numeric(fixef(model)),
-                   tau.e = 1/data.frame(VarCorr(model))[2, "sdcor"]^2,
-                   tau.b0 = 1/data.frame(VarCorr(model))[1, "sdcor"]^2)
+# Save JAGS model specification to a file
+writeLines(model_code, "lmm.model.txt")
+
+
+# Initialize parameters for JAGS
+initials = list( list(b0 = t(ranef(model)$player)[1:G], 
+                      beta = as.numeric(fixef(model)))
 )
 
-params = c("beta","sigma.e","sigma.b0", "b0")
+params = c("beta","b0")
 
+# Prepare data for JAGS
+data.input <- list(
+  n = n,
+  K = K,
+  G = G,
+  y = as.numeric(barca_shot$goal) - 1,  # Convert "goal" to numeric 0/1 values
+  shot_distance = barca_shot$shot_distance,
+  shot_angle = barca_shot$shot_angle,
+  bodypart = as.numeric(as.factor(barca_shot$bodypart)),
+  technique = as.numeric(as.factor(barca_shot$technique)),
+  first_touch = as.numeric(as.factor(barca_shot$first_touch)),
+  prefered_type = as.numeric(as.factor(barca_shot$prefered_type)),
+  inside_18 = as.numeric(as.factor(barca_shot$inside_18)),
+  time = barca_shot$time,
+  player = as.numeric(as.factor(barca_shot$player))
+)
 
+# Run JAGS model
+true.model.jags <- jags(
+  data = data.input,
+  inits = inits,
+  parameters.to.save = params,
+  model.file = "lmm.model.txt",
+  DIC = TRUE,
+  n.chains = 1,
+  n.iter = 20000,
+  n.burnin = 1100,
+  n.thin = 2
+)
 
-data.input = list(n = n, K = K, G = groups , 
-                  y = barca_shot$goal , distance = barca_shot$shot_distance,
-                  angle = barca_shot$shot_angle, body_part = barca_shot$bodypart,
-                  tecnique = barca_shot$technique, first_touch = barca_shot$first_touch,
-                  preferred_type = barca_shot$prefered_type, inside_18 = barca_shot$inside_18,
-                  time = barca_shot$time)
 
 
