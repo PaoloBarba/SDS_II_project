ab8e6c02681050fefc43099baa163d4933ba1961
PaoloBarba <114430394+PaoloBarba@users.noreply.github.com>
2023-06-27 16:33:26 +0200

    mannaggia

--- Progetto_SDS.R
+++ Progetto_SDS.R
@@ -28,38 +28,52 @@ colnames(barca_shot)
 library(lme4)
 library(R2jags)
 
-# Fit the generalized linear mixed effects model (GLMM)
-model <- glmer(as.factor(goal) ~ (1|player) + scale(shot_distance) + scale(shot_angle) + bodypart +
-                 technique + first_touch + prefered_type + inside_18 + scale(time), 
-               data = barca_shot, family = binomial)
 
+attach(barca_shot)
+player = as.numeric(as.factor(barca_shot$player))
+X = model.matrix(~ 1 + shot_distance  + shot_angle + bodypart + technique + first_touch + prefered_type + inside_18 + time + player)
+detach(barca_shot)
+X = as.data.frame(X)
+colnames(X)
+
+
+# Fit the generalized linear mixed effects model (GLMM)
+model <- glmer(as.factor(barca_shot$goal) ~ (1|player) + scale(shot_distance) + scale(shot_angle) + bodypart +
+                 technique + first_touch + prefered_type + inside_18 + scale(time), 
+               data = X, family = binomial)
 
 
 # Obtain the number of observations, groups, and parameters
 n <- nrow(barca_shot)
 G <- length(unique(barca_shot$player))
-K <- 12  # Number of fixed effects parameters
+K <- length(fixef(model))
 
 # Write JAGS model specification to a file
 model_code <- "
 model {
   for(k in 1:K){ 
-	beta[k] ~ dnorm(0,0.001)   # prior for beta
+	beta[k] ~ dnorm(0,0.001)  
 }	
 
-# Statistical (conditional) model
-for (j in 1:G) {
+  tau.e ~ dgamma(9,.5)
+  sigma.e <- 1/sqrt(tau.e)
 
-  b0[j] ~ dnorm(0, tau.b0) # random intercept for each ranked player
-}
+  tau.b0 ~ dgamma(9,0.001)
+  sigma.b0 <- 1/sqrt(tau.b0)
+
+  for (j in 1:G) {
+   b0[j] ~ dnorm(0, tau.b0)  
+  }
 
 
   for (i in 1:n) {
-    mu[i] <- beta[1] + b0[player[i]] + beta[2] * shot_distance[i] + beta[3] * shot_angle[i] + beta[4] * bodypart[i] + 
-              beta[5] * technique[i] + beta[6] * first_touch[i] + beta[7] * prefered_type[i] + 
-              beta[8] * inside_18[i] + beta[9] * time[i]
-    y[i] ~ dbin(p[i], 1)  # Likelihood function with binomial distribution
-    p[i] <- 1 / (1 + exp(-mu[i]))  # Link function (logit)
+    mu[i] <- beta[1] + b0[player[i]] + beta[2] * shot_distance[i] + 
+    beta[3] * shot_angle[i] + beta[4] * bodypartLeft_Foot[i] + beta[5] *  bodypartOther_Bodypart[i] + 
+    beta[6] * bodypartRight_Foot[i] + beta[7] * techniqueVolley[i] + beta[8] * first_touchTrue[i] +
+    beta[9] * prefered_typeLeft_Foot[i] + beta[10] * prefered_typeRight_Foot[i] +
+    beta[11] * inside_18True[i] + beta[12] * time[i]
+    p[i] <- 1 / (1 + exp(-mu[i]))  
+    y[i] ~ dbin(p[i], 1)           
   }
 }"
 
@@ -67,29 +81,37 @@ for (j in 1:G) {
 writeLines(model_code, "lmm.model.txt")
 
 
-# Initialize parameters for JAGS
-initials = list( list(b0 = t(ranef(model)$player)[1:G], 
-                      beta = as.numeric(fixef(model)))
+inits = list( list(b0 = t(ranef(model)$player)[1:G], 
+                   beta = as.numeric(fixef(model)),
+                   tau.e = 1,
+                   tau.b0 = 1)
 )
 
-params = c("beta","b0")
+
+params = c("beta","sigma.e","sigma.b0", "b0")
 
 # Prepare data for JAGS
-data.input <- list(
-  n = n,
-  K = K,
-  G = G,
-  y = as.numeric(barca_shot$goal) - 1,  # Convert "goal" to numeric 0/1 values
-  shot_distance = barca_shot$shot_distance,
-  shot_angle = barca_shot$shot_angle,
-  bodypart = as.numeric(as.factor(barca_shot$bodypart)),
-  technique = as.numeric(as.factor(barca_shot$technique)),
-  first_touch = as.numeric(as.factor(barca_shot$first_touch)),
-  prefered_type = as.numeric(as.factor(barca_shot$prefered_type)),
-  inside_18 = as.numeric(as.factor(barca_shot$inside_18)),
-  time = barca_shot$time,
-  player = as.numeric(as.factor(barca_shot$player))
-)
+data.input <-  list(n = n,
+                    K = K, 
+                    G = G, 
+                    y = barca_shot$goal,
+                    shot_distance = X$shot_distance,
+                    shot_angle = X$shot_angle,
+                    bodypartLeft_Foot = X$`bodypartLeft Foot`,
+                    bodypartOther_Bodypart = X$`bodypartOther Bodypart`,
+                    bodypartRight_Foot = X$`bodypartRight Foot`,
+                    techniqueVolley = X$techniqueVolley,
+                    first_touchTrue = X$first_touchTrue,
+                    prefered_typeLeft_Foot = X$`prefered_typeLeft Foot`,
+                    prefered_typeRight_Foot = X$`prefered_typeRight Foot`,
+                    inside_18True = X$inside_18True,
+                    time = X$time,
+                    player = X$player
+                  )
+    
+                  
+rjags::load.module('dic')
+
 
 # Run JAGS model
 true.model.jags <- jags(
@@ -103,6 +125,5 @@ true.model.jags <- jags(
   n.burnin = 1100,
   n.thin = 2
 )
-
 
 
